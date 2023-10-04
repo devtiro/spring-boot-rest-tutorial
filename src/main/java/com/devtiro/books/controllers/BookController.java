44c4e238d62100101ec5d02fa85775c5f254ce38
Mihai <kubikweg@gmail.com>
2023-10-04 23:09:48 +0300

    Unused type arguments for the non-generic method status(HttpStatusCode) of type ResponseEntity; it should not be parameterized with arguments <Book>," is due to unnecessary type arguments in the ResponseEntity creation.
    Removing the type arguments <Book>
    
    Second:
    BookController class with the <T> type parameter correctly parameterized for each ResponseEntity:

--- src/main/java/com/devtiro/books/controllers/BookController.java
+++ src/main/java/com/devtiro/books/controllers/BookController.java
@@ -7,12 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BookController {
@@ -28,33 +23,26 @@ public class BookController {
   public ResponseEntity<Book> createUpdateBook(
       @PathVariable final String isbn, @RequestBody final Book book) {
     book.setIsbn(isbn);
-
     final boolean isBookExists = bookService.isBookExists(book);
     final Book savedBook = bookService.save(book);
 
-    if (isBookExists) {
-      return new ResponseEntity<Book>(savedBook, HttpStatus.OK);
-    } else {
-      return new ResponseEntity<Book>(savedBook, HttpStatus.CREATED);
-    }
+    return ResponseEntity.status(isBookExists ? HttpStatus.OK : HttpStatus.CREATED).body(savedBook);
   }
 
   @GetMapping(path = "/books/{isbn}")
   public ResponseEntity<Book> retrieveBook(@PathVariable final String isbn) {
     final Optional<Book> foundBook = bookService.findById(isbn);
-    return foundBook
-        .map(book -> new ResponseEntity<Book>(book, HttpStatus.OK))
-        .orElse(new ResponseEntity<Book>(HttpStatus.NOT_FOUND));
+    return foundBook.map(book -> ResponseEntity.ok(book)).orElse(ResponseEntity.notFound().build());
   }
 
   @GetMapping(path = "/books")
   public ResponseEntity<List<Book>> listBooks() {
-    return new ResponseEntity<List<Book>>(bookService.listBooks(), HttpStatus.OK);
+    return ResponseEntity.ok(bookService.listBooks());
   }
 
   @DeleteMapping(path = "/books/{isbn}")
-  public ResponseEntity deleteBook(@PathVariable final String isbn) {
+  public ResponseEntity<Void> deleteBook(@PathVariable final String isbn) {
     bookService.deleteBookById(isbn);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/com/devtiro/books/repositories/BookRepository.java
+++ src/main/java/com/devtiro/books/repositories/BookRepository.java
@@ -2,9 +2,7 @@ package com.devtiro.books.repositories;
 
 import com.devtiro.books.domain.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository
-    extends JpaRepository<BookEntity, String>, PagingAndSortingRepository<BookEntity, String> {}
+public interface BookRepository extends JpaRepository<BookEntity, String> {}
